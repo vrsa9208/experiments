5aeb5b9a783bd850a8c02ad78c032d31cad1dcd2
vrsa9208 <vrsa9208@gmail.com>
2018-08-30 22:48:03 -0500

    Comparing Integers

---
+++ src/main/java/com/vrsa9208/experiments/CompareIntegers.java
@@ -0,0 +1,14 @@
+package com.vrsa9208.experiments;
+
+public class CompareIntegers {
+
+	public static void main(String[] args) {
+		Integer a = 42;
+		Integer b = 42;
+		System.out.println(a == b);
+		Integer c = 666;
+		Integer d = 666;
+		System.out.println(c == d);
+	}
+
+}
